7017c05d5e0b6df2b087257b8c2c43f2962990cf
Muhammad Ghifar <75981965+muhammadghifar@users.noreply.github.com>
2024-06-13 14:04:10 +0700

    add 8kyu js training

---
+++ JS/kyu-8/what-is-between.js
@@ -0,0 +1,10 @@
+// https://www.codewars.com/kata/55ecd718f46fba02e5000029
+function between(a, b) {
+  // your code here
+  let arr = []
+  for(let i = a; i<=b; i++) {
+    arr.push(i)
+  }
+  
+  return arr
+}
